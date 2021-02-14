7c93c237438acbdf27e79a010a7e727e271d9587
noppakan <Noppakansupa007@gmail.com>
2021-02-14 16:53:52 +0700

    จบ laravel ไม่เสร้จ

---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.master')
+
+@section('content')
+    <h1>กรุณากรอกข้อมูล</h1>
+    <form action="/user/update/{{$user->id}}" method="post">
+        @csrf
+        <input type="text"name = "name" value="{{$user->name}}" required>
+        <input type="text"name = "username" value="{{$user->username}}" required>
+        <input type="text"name = "email" value="{{$user->email}}" required>
+        <input type="text"name = "password" value="{{$user->password}}" required>
+        <button type="submit">บันทึก</button>
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,10 +33,23 @@ Route::get('/category/edit/{id}', [CategoryController::class, 'edit']);
 Route::post('/category/update/{id}', [CategoryController::class, 'update']);
 Route::get('/category/delete/{id}', [CategoryController::class, 'delete']);
 
-Route::get('/category', [UserController::class, 'index']);
-Route::get('/category/create', [UserController::class, 'create']);
-Route::post('/category/store', [UserController::class, 'store']);
-Route::get('/category/edit/{id}', [UserController::class, 'edit']);
-Route::post('/category/update/{id}', [UserController::class, 'update']);
-Route::get('/category/delete/{id}', [UserController::class, 'delete']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/create', [UserController::class, 'create']);
+Route::post('/user/store', [UserController::class, 'store']);
+Route::get('/user/edit/{id}', [UserController::class, 'edit']);
+Route::post('/user/update/{id}', [UserController::class, 'update']);
+Route::get('/user/delete/{id}', [UserController::class, 'delete']);
+
+Route::get('/post', [PostController::class, 'index']);
+Route::get('/post/create', [PostController::class, 'create']);
+Route::post('/post/store', [PostController::class, 'store']);
+Route::get('/post/edit/{id}', [PostController::class, 'edit']);
+Route::post('/post/update/{id}', [PostController::class, 'update']);
+Route::get('/post/delete/{id}', [PostController::class, 'delete']);
+
+Route::get('/auth/login', [AuthController::class, 'login'])->name("login");
+Route::post('/auth/doLogin', [AuthController::class, 'doLogin']);
+Route::get('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/doRegister', [AuthController::class, 'doRegister']);
+Route::get('/auth/logout', [AuthController::class, 'logout']);
 
